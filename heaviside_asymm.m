c598b1c20ec0c5388d2dfd5ac69d7774ae17b7dd
David Owen <david.owen.14@ucl.ac.uk>
2014-10-10 12:05:18 +0100

    Fixed function to start at TE_min and end at TE_max.
    Prevents noise from being added to y values that are definitely zero.
    A new Heaviside function was implemented to prevent the skewing of
    results: MATLAB's built-in 'heaviside(x)' returns 1/2 at x=0.

---
+++ heaviside_asymm.m
@@ -0,0 +1,10 @@
+function output = heaviside_asymm( x )
+% heaviside_asymm(x), returns 1 iff x>=0, returns 0 otherwise
+if x>=0
+    output = 1;
+else
+    output = 0;
+end
+
+end
+
--- nlls.m
+++ nlls.m
@@ -6,10 +6,11 @@
 clear all;
 
 % independent variable is time
-TEs = [0:10E-3:160E-3];
 
 TE_min = 50E-3;
 TE_max = 150E-3;
+
+TEs = [TE_min:10E-3:TE_max];
 
 % grey, white, csf
 T2  = [110E-3; 80E-3; 400E-3];
@@ -19,7 +20,7 @@ v_t = [0.7; 0.2; 0.1];
 
 % define func to create data and test against
 func = @(v)( (v(1)*exp(-TEs/T2(1)) + v(2)*exp(-TEs/T2(2)) + v(3)*exp(-TEs/T2(3))) ...
-              .* heaviside(TEs - TE_min) .* heaviside(TE_max - TEs) );
+              .* heaviside_asymm(TEs - TE_min) .* heaviside_asymm(TE_max - TEs) );
 
 % generate noisy data -- don't bother saving the noise, though
 % TODO: frame this in terms of SNR
